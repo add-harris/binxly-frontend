d9b60c70aff4011fd17fa652ddf3b9e99d94c6a0
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2021-01-18 22:45:02 +0000

    - added success / fail icons - implemeted form - added form validation

--- pages/index.vue
+++ pages/index.vue
@@ -6,28 +6,58 @@
           Login
         </v-card-title>
 
-        <v-text-field
-          class="pl-10 pr-10"
-          label="username"
-          v-model="username"
-        ></v-text-field>
-
-        <v-text-field
-          class="ml-10 mr-10"
-          type="password"
-          label="password"
-          v-model="password"
-        ></v-text-field>
-        <v-row
-          align="center"
-          justify="space-around"
+        <v-form
+          ref="form"
+          lazy-validation
         >
 
-        <v-card-actions>
-          <v-btn color="primary" @click="signIn">Login</v-btn>
-        </v-card-actions>
+          <v-text-field
+            class="pl-10 pr-10"
+            label="email"
+            v-model="email"
+            :rules="emailRules"
+            validate-on-blur
+            required
+          >
+            <v-icon
+              :color="iconColor"
+              slot="append"
+            >
+              {{showIcon}}
+            </v-icon>
+          </v-text-field>
 
-        </v-row>
+          <v-text-field
+            class="ml-10 mr-10"
+            type="password"
+            label="password"
+            v-model="password"
+            :rules="passwordRules"
+            validate-on-blur
+            required
+          >
+            <v-icon
+              :color="iconColor"
+              slot="append"
+            >
+              {{showIcon}}
+            </v-icon>
+
+          </v-text-field>
+
+          <v-row
+            align="center"
+            justify="space-around"
+          >
+
+            <v-card-actions>
+              <v-btn color="primary" @click="checkValid">Login</v-btn>
+            </v-card-actions>
+
+          </v-row>
+
+        </v-form>
+
       </v-card>
     </v-col>
   </v-row>
@@ -41,23 +71,49 @@ export default {
   },
   data() {
     return {
-      username: "",
-      password: ""
+
+      email: "",
+      password: "",
+      showIcon: "",
+      iconColor: "green",
+      successIcon: "mdi-account-check",
+      failIcon: "mdi-account-remove",
+
+      emailRules: [
+        v => !!v || 'email is required',
+        v => /.+@.+/.test(v) || 'email must be valid',
+      ],
+      passwordRules: [
+        v => !!v || 'password is required',
+        v => v.length >= 6 || 'password must be at least 6 characters',
+      ],
+
     }
   },
   methods: {
+
+    checkValid() {
+      if (this.$refs.form.validate()) {
+        console.log("form is vaild")
+        this.signIn()
+      } else {
+        console.log("form is not vaild")
+      }
+    },
+
     signIn() {
 
-      let email = "em123@bmail.com"
-      let pass = "123456"
+      console.log(`email: ${this.email}, password: ${this.password}`)
 
-      console.log(`email: ${email}, password: ${pass}`)
-
-      this.$fireModule.auth().signInWithEmailAndPassword(email, pass)
+      this.$fireModule.auth().signInWithEmailAndPassword(this.email, this.password)
         .then((user) => {
+          this.iconColor = "green"
+          this.showIcon = this.successIcon
           console.log("success")
         })
         .catch((error) => {
+          this.iconColor = "red"
+          this.showIcon = this.failIcon
           console.log("fail")
         });
 
